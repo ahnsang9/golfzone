a6bdc326341cef8a8863f1bc771373e6fb7ca8fc
ahnsanghoon <40432401+ahnsang9@users.noreply.github.com>
2020-10-26 21:40:16 +0900

    2

--- 대회순위.py
+++ 대회순위.py
@@ -3,6 +3,7 @@ from time import sleep
 from datetime import datetime
 import pandas as pd
 import openpyxl
+import re
 
 if __name__ == '__main__':
     def click(xpath):
@@ -21,8 +22,9 @@ if __name__ == '__main__':
         score_correction = driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div/div/div[3]/div[2]/div[3]/table/tbody/tr/td[7]/a')
         final_score = driver.find_elements_by_xpath('/html/body/div[2]/div[3]/div/div/div[3]/div[2]/div[3]/table/tbody/tr/td[8]')
         for i in range(len(rank)):
-            info[0].append(rank[i].text.replace('T', ''))
-            info[1].append(nickname[i].text)
+            info[0].append(rank[i].text.replace('T', '')+'위')
+            #info[1].append((re.sub(r'\(.*?\)', '', nickname[i].text)).replace("\n",""))
+            info[1].append((nickname[i].text).replace("\n", " "))
             info[2].append(round[i].text)
             info[3].append(a[i].text)
             info[4].append(b[i].text)
@@ -30,7 +32,7 @@ if __name__ == '__main__':
             info[6].append(final_score[i].text)
         return info
 
-    driver_path = 'C:\\Users\82105\PycharmProjects\대회순위\chromedriver.exe'
+    driver_path = 'chromedriver.exe'
     url = 'http://m.golfzon.com/ghome/#!/tournament/1/14042'
     info = [[], [], [], [], [], [], []]   # 0.순위 1.닉네임 2.라운드수 3.A코스 4.B코스 5.스코어보정치 6.최종성적
     info2 = [[], [], [], [], [], [], []]  # 多라운드
@@ -66,8 +68,7 @@ if __name__ == '__main__':
     b_course = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div/div[3]/table/tbody/tr[6]/td/ul/li[2]').text[5:]
     driver.quit()
 
-
-    wb = openpyxl.load_workbook('C:\\Users\82105\Desktop\\매장대회.xlsx')
+    wb = openpyxl.load_workbook('매장대회.xlsx')
     temp = wb.active
 
     ################################################################# 순위
@@ -79,7 +80,7 @@ if __name__ == '__main__':
     df = pd.DataFrame({'rank': info2[2][:5]})
     df['rank_min'] = df['rank'].rank(method='min', ascending=False)
     for i in range(min(len(info2[1]), 5)):
-        temp.cell(row=i+29,column=1).value = int(df['rank_min'][i])
+        temp.cell(row=i+29,column=1).value = str(int(df['rank_min'][i]))+'위'
         temp.cell(row=i+29, column=2).value = info2[1][i]
         temp.cell(row=i+29, column=7).value = info2[2][i]
 
@@ -87,6 +88,6 @@ if __name__ == '__main__':
     temp.cell(row=26, column=1).value = title2
     temp.cell(row=3, column=4).value = a_course
     temp.cell(row=3, column=5).value = b_course
-    wb.save('C:\\Users\82105\Desktop\\매장대회(%d월%d일).xlsx' % (datetime.now().month, datetime.now().day))
+    wb.save('매장대회(%d월%d일).xlsx' % (datetime.now().month, datetime.now().day))
 
 
